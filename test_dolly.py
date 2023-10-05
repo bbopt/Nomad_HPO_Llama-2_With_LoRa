d316d654680908374842131065b06025c82fddd7
Sacha Benarroch-Lelong <sacha.benarroch@gmail.com>
2023-10-05 17:31:19 -0400

    temp test dolly

---
+++ test_dolly.py
@@ -0,0 +1,6 @@
+import random
+import os
+
+for file in random.sample(os.listdir("../lora_output/"), k=10):
+    cmd = f'CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun test_eval_with_LoRa.py --model_name_or_path decapoda-research/llama-7b-hf --data_path databricks/databricks-dolly-15k --lora_path {file} --output_dir ./output_dir --bf16 True --do_eval True --per_device_eval_batch_size 30 --do_train False --gradient_accumulation_steps 8 --evaluation_strategy "steps" --save_strategy "steps" --save_steps 2000 --save_total_limit 1 --logging_steps 1 --tf32 True'
+    os.system(cmd)
