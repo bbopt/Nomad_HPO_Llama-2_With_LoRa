b95d142420ded2cbf542374e33308e0c13dde5a3
Sacha Benarroch-Lelong <sacha.benarroch@gmail.com>
2023-11-17 17:42:54 -0500

    [blind-eval] add results

--- blind_eval/pipeline.py
+++ blind_eval/pipeline.py
@@ -1,6 +1,6 @@
 from transformers import LlamaForCausalLM, LlamaTokenizer
 from peft import PeftModelForCausalLM
-from typing import Optional, Iterable
+from typing import Optional, Dict
 import numpy as np
 from tqdm import tqdm
 from logzero import logger
@@ -8,41 +8,25 @@ import json
 
 from fire import Fire
 
-
-def outputs_to_json(X: np.ndarray, filepath: str) -> None:
-    """
-    Exports the model output to a JSON file of format
-    ```
-    [
-        {"question": question, "output": output} for (question, output) in zip(questions, outputs)
-    ]
-    ```
-    """
-    assert (
-        X.shape[1] == 2
-    ), "Uncoherent shape to export model outputs. The outputs should have 2 columns (one for the question, one for the answer), passed {}".format(
-        X.shape[1]
-    )
-
-    outputs = [{"question": question.strip(), "output": output.strip()} for (question, output) in X]
-
-    with open(filepath, "w") as f:
-        json.dump(outputs, f)
-
-    return None
-
-
 def read_questions_file(
     questions_file: str, filetype: Optional[str] = "txt"
-) -> Iterable[str]:
+) -> Dict[str, str]:
     """
     Reads the question with the appropriate format, and provides an iterable over all questions.
     """
     if filetype == "txt":
         with open(questions_file, "r") as f:
             lines = f.readlines()
+        return [{"question_id": i, "text": line} for i, line in enumerate(lines)]
 
-    return lines
+    if filetype == "jsonl":
+        questions = []
+        with open(questions_file, "r") as f:
+            lines = f.readlines()
+        for line in lines:
+            line = json.loads(line)
+            questions.append({"question_id": line["question_id"], "text": line["text"]})
+        return questions
 
 
 def generate_from_prompt(
@@ -53,18 +37,21 @@ def generate_from_prompt(
     max_tokens: int,
 ) -> str:
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
-    outputs = model.generate(**inputs, max_new_tokens=max_tokens)
+    outputs = model.generate(
+        **inputs, max_new_tokens=max_tokens, do_sample=True, temperature=0.7
+    )
     length = inputs.input_ids.shape[1]
 
-    return tokenizer.decode(outputs[0, length:], skip_special_tokens=True)
+    return tokenizer.decode(outputs[0, length:], skip_special_tokens=True).strip()
 
 
 def main(
     questions_file: str,
     output_file: str,
+    filetype: str,
     lora_path: Optional[str] = None,
     device: Optional[str] = "cuda",
-    max_tokens: Optional[int] = 512,
+    max_tokens: Optional[int] = 1024,
 ) -> None:
     assert output_file.endswith(
         ".json"
@@ -75,23 +62,29 @@ def main(
 
     if lora_path is not None:
         model = PeftModelForCausalLM.from_pretrained(model, lora_path)
-        model.to(device)
+
+    model.to(device)
 
     logger.info("Model has been loaded on device {device}.".format(device=device))
 
-    questions = read_questions_file(questions_file)
+    questions = read_questions_file(questions_file, filetype)
 
     outputs = []
 
     logger.debug("Answering questions...")
     for question in tqdm(questions):
-        output = generate_from_prompt(question, model, tokenizer, device, max_tokens)
-        outputs.append([question, output])
+        output = generate_from_prompt(question["text"], model, tokenizer, device, max_tokens)
+        outputs.append(
+            {
+                "question_id": question["question_id"],
+                "text": question["text"],
+                "output": output,
+            }
+        )
 
-    # Export outputs to CSV
-    outputs = np.array(outputs)
-
-    outputs_to_json(outputs, output_file)
+    # Export outputs to JSON
+    with open(output_file, "w") as f:
+        json.dump(outputs, f)
 
     logger.info("Answers successfully saved.")
 
